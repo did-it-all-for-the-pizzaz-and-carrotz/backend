bf57ef96dab4efe6711e10db7c202feb00d4f2e6
Mateusz Jamróz <jamr.mat@gmail.com>
2023-03-05 13:28:30 +0100

    init

---
+++ src/test/java/tech/carrotly/restapi/restapi/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.carrotly.restapi.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
