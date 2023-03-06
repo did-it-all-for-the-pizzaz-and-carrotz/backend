d70ad227198578e87a2668882f9dc1502aead83f
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2023-03-06 01:35:47 +0100

    cleanup

--- src/main/java/tech/carrotly/restapi/service/impl/ChatServiceImpl.java
+++ src/main/java/tech/carrotly/restapi/service/impl/ChatServiceImpl.java
@@ -32,14 +32,6 @@ public class ChatServiceImpl implements ChatService {
     private final Set<Connection> users = new HashSet<>();
     private final AssistantService assistantService;
 
-//    private Map<String, Function<?, ?>> actionMap = new HashMap<>(Map.of(
-//            "createChatroom", (payload) -> createChatroom(), // wysylamy do wszystkich zalogowanych ziutow popupa
-//            "removeChatroom", (payload) -> removeChatroom((String) payload), //wysylamy do lekarza payload zeby na froncie wywalilo powiadomienie o zamknieciu roomu i do reszty jako koniec
-//            "helperEnteredChatroom", (payload) -> helperEnteredChatroom(payload), // front dostaje popupa ze ktos dolaczyl, room znika z mozliwosci dolaczenia dla lekarzy
-//            "helperLeftChatroom", (payload) -> helperLeftChatroom(payload), // front dostaje popupa ze ktos wyszedl, room pokazuje sie na nowo dla lekarzy (15 sekund czekania) -> popup i tryb AI
-//            "onMessage", (payload) -> onMessage(payload), // wyslanie wiadomosci do uzytkownikow chatroomu
-//    ));
-
     public UUID createChatroom(WebSocket conn) {
         final UUID chatroomUuid = UUID.randomUUID();
 
